0ae827abf58117822008842db29e8c9fde4ec089
AyseSaricoban <aysesrcbnn@gmail.com>
2024-01-14 13:05:27 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,11 @@ public class Main {
         System.out.println(isPerfectNumber(28));
         System.out.println(isPerfectNumber(5));
         System.out.println(isPerfectNumber(-1));
+        System.out.println("-------------------------");
+        System.out.println(numberToWords(123));
+        System.out.println(numberToWords(1010));
+        System.out.println(numberToWords(-12));
+        System.out.println(numberToWords(11));
     }
     public static boolean isPalindrome (int number){
         number = Math.abs(number);
@@ -19,7 +24,63 @@ public class Main {
         }
         return reversed.equals(String.valueOf(number));
     }
-    public static boolean isPerfectNumber (int number){
-
+    public static boolean isPerfectNumber (int num){
+        if (num <= 5) {
+            return false;
+        }
+        int total = 1;
+        int highest = (int) Math.sqrt(num);
+        highest++;
+        for (int i = 2; i < highest; i++) {   // i = 1 i<= 3      i = 2 num = 3  i = 3 num= 3 i= 4 num = 3
+            if (num % i == 0) {
+                total += i;
+                if (i != num / i) {
+                    total += num / i;
+                }
+            }
+        }
+        return num == total;
+    }
+    public static String numberToWords(int num) {
+        if (num < 0) {
+            return "Invalid Value";
+        }
+        char[] digits = String.valueOf(num).toCharArray();
+        String numToText = "";
+        for (char digit : digits) { // digits 1,1  digit = 1
+            switch (digit) {
+                case '0':
+                    numToText += "Zero ";
+                    break;
+                case '1':
+                    numToText += "One ";
+                    break;
+                case '2':
+                    numToText += "Two ";
+                    break;
+                case '3':
+                    numToText += "Three ";
+                    break;
+                case '4':
+                    numToText += "Four ";
+                    break;
+                case '5':
+                    numToText += "Five ";
+                    break;
+                case '6':
+                    numToText += "Six ";
+                    break;
+                case '7':
+                    numToText += "Seven ";
+                    break;
+                case '8':
+                    numToText += "Eight ";
+                    break;
+                case '9':
+                    numToText += "Nine ";
+                    break;
+            }
+        }
+        return numToText.trim();
     }
 }
